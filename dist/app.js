6c2a9879f9f135a17820d9727d5387e049cf99f8
BryanCachimuel <bryanloyo56@gmail.com>
2022-07-04 21:05:01 -0500

    instalando y configurando en el servidor la dependencia morgan

--- dist/app.js
+++ dist/app.js
@@ -6,9 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // lo que esta entre { Application }  dice que va a importar la interfaz de la aplicación
 const express_1 = __importDefault(require("express"));
 const auth_1 = __importDefault(require("./routes/auth"));
+const morgan_1 = __importDefault(require("morgan"));
 const app = (0, express_1.default)();
 // configuraciones
 app.set('port', 3009);
+// middlewares -> se debe poner la configuració dev de desarrollo
+app.use((0, morgan_1.default)('dev'));
 // rutas
 app.use(auth_1.default);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 // lo que esta entre { Application }  dice que va a importar la interfaz de la aplicación
 import express, {Application} from 'express';
 import authRoutes from './routes/auth';
+import morgan from 'morgan';
 
 const app: Application = express();
 
 // configuraciones
 app.set('port', 3009);
+
+// middlewares -> se debe poner la configuració dev de desarrollo
+app.use(morgan('dev'));
 
 // rutas
 app.use(authRoutes)
