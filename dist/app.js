cc8a0c6d1609a6b7ea5890d89c5c25e4c8527a07
BryanCachimuel <bryanloyo56@gmail.com>
2022-07-04 20:57:51 -0500

    creando el directorio y archivo para las rutas del proyecto y configurando las mismas en el servidor

--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // lo que esta entre { Application }  dice que va a importar la interfaz de la aplicación
 const express_1 = __importDefault(require("express"));
+const auth_1 = __importDefault(require("./routes/auth"));
 const app = (0, express_1.default)();
 // configuraciones
 app.set('port', 3009);
+// rutas
+app.use(auth_1.default);
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 // lo que esta entre { Application }  dice que va a importar la interfaz de la aplicación
 import express, {Application} from 'express';
+import authRoutes from './routes/auth';
 
 const app: Application = express();
 
 // configuraciones
 app.set('port', 3009);
+
+// rutas
+app.use(authRoutes)
 
 export default app;
