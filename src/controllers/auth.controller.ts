8a329f19ab9f775e58a00fe81034c31d0d8240f3
BryanCachimuel <bryanloyo56@gmail.com>
2022-07-05 19:21:53 -0500

    creando la funcionalidad del método para registrar usuarios

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express'
+import User, { IUser } from '../models/User'
 
-export const signup = (req: Request, res: Response) => {
-    res.send('signup')
+
+export const signup = async (req: Request, res: Response) => {
+    const user: IUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+    const saveuser = await user.save()
+    console.log(saveuser)
+    res.send('usuario registrado correctamente')
 }
 
 export const signin = (req: Request, res: Response) => {
