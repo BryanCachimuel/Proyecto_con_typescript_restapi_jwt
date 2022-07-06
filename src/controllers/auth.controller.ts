a0c1e9b1fdf093ac33ab103ff994b9219bb5828b
BryanCachimuel <bryanloyo56@gmail.com>
2022-07-05 21:56:55 -0500

    creando el método para iniciar sesión para los usuarios y creando un token cada vez que inicia sesión

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -20,8 +20,19 @@ export const signup = async (req: Request, res: Response) => {
     res.header('auth-token', token).json(saveUser);
 }
 
-export const signin = (req: Request, res: Response) => {
-    res.send('signin')
+export const signin = async (req: Request, res: Response) => {
+    const user = await User.findOne({email: req.body.email})
+    if(!user) return res.status(400).json('email o contraseña incorrectos');
+    
+    const correctPassword: boolean = await user.validatePassword(req.body.password);
+    if(!correctPassword) return res.status(400).json('Contraseña no valida')
+
+    // generando token una vez el usuario inicie sesión y también se le agrega un tiempo de expiración
+    const token: string = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET || 'tokentest', {
+        expiresIn: 60 * 60 *24
+    });
+
+    res.header('auth-token', token).json(user)
 }
 
 export const profile = (req: Request, res: Response) => {
